3f422af00db47f9657903b187e25bce00598033b
Luca Ribeiro <luca.rsr.luca@gmail.com>
2021-03-02 09:35:43 -0300

    v2.0

--- script.ps1
+++ script.ps1
@@ -1,48 +1,104 @@
+# Seta o caminho de saída do arquivo 
+$PastaCompartilhada = Get-Location
 # Informações sobre as máquinas 
 $HostName = ${env:COMPUTERNAME}
 $User = ${env:USERNAME}
-$IP = (Get-NetIPAddress -AddressFamily IPv4 -PrefixOrigin Dhcp).IPAddress
-# Informações sobre o Windows Defender
-$DefenderStatus = Get-MpComputerStatus
+$IPList = ''
+$AllIP = Get-NetIPAddress -AddressFamily IPv4
+$AllIP  | ForEach-Object {
+    if ($_.PrefixOrigin -ne "WellKnow" -and $_.InterfaceAlias -notlike "*WSL*" -and $_.InterfaceAlias -notlike "*VM*") {
+        $IPList += $_.InterfaceAlias + ': ' + $_.IPAddress + '; ' 
+    }
+}
 # Nome do sistema operacional
-$OS = (Get-ComputerInfo -Property OSName).OSName
-# Data da última atualização
-$LastUpdate = ((Get-HotFix -Description Update | Sort-Object InstalledOn -Descending)[0]).InstalledOn
-# Data da última atualização de segurança
-$LastSecurityUpdate = ((Get-HotFix -Description Security* | Sort-Object InstalledOn -Descending)[0]).InstalledOn
-# Lista com HotFix's Instalados
+try {
+    $OS = (Get-ComputerInfo -Property OSName -ErrorAction Stop).OSName 
+}
+catch {
+    $OS = ((Get-WmiObject Win32_OperatingSystem).Name).split('|')[0]
+}   
+# Informações sobre o Antivírus
+#try {
+#    $EndpointSecurity = 'Defender'
+#    $DefenderStatus = Get-MpComputerStatus -ErrorAction Stop
+#}
+#catch {
+#    $EndpointSecurity = (Get-WmiObject -Namespace root/SecurityCenter2 -ClassName AntivirusProduct)[-1].displayName
+#    $DefenderStatus = $null
+#}
+try {
+    $DefenderStatus = Get-MpComputerStatus -ErrorAction Stop
+}
+catch {
+    $DefenderStatus = $null
+}
+# Lista HotFix's Instalados e ordena por data de instalação
+$AllHotFix = (Get-HotFix | Sort-Object -Property InstalledOn)
+$UpdateList = ($AllHotFix | Where-Object -Property Description -Like 'Update')
+$SecurityUpdateList = ($AllHotFix | Where-Object -Property Description -Like 'Security Update')
+# Seta valores das variáveis
 $HotFixList = ''
-Get-HotFix | ForEach-Object { if ($HotFixList -eq ''){$HotFixList += $_.HotFixID}else { $HotFixList += ', ' + $_.HotFixID } }
+$LastUpdate = 'Sem atualizacoes'
+$LastSecurityUpdate = 'Sem atualizacoes'
 
+if ($null -ne $AllHotFix) {
+    if ($null -ne $UpdateList) {
+        # Data da última atualização
+        $LastUpdate = $UpdateList[-1].InstalledOn
+    }
+    if ($null -ne $SecurityUpdateList) {
+        # Data da última atualização de segurança
+        $LastSecurityUpdate = $SecurityUpdateList[-1].InstalledOn
+    }
+
+    ForEach ($HotFix in $AllHotFix) { 
+        if ($HotFixList -eq '') {
+            $HotFixList += $HotFix.HotFixID 
+        }
+        else {
+            $HotFixList += ', ' + $HotFix.HotFixID 
+        } 
+    }
+}
+
+# Cria o objeto de saída com as informações coletadas
 $Ouptut = [PSCustomObject]@{
     # Computador
     'Computador'                       = $HostName 
     'Usuario'                          = $User
-    'IP'                               = $IP
+    'IP'                               = $IPList
     'Sistema Operacional'              = $OS
+
     # Gerais - Defender
     'Versao do Defender'               = $DefenderStatus.AMProductVersion
     'versao de Engenharia do Defender' = $DefenderStatus.AMEngineVersion
     'Versao de servico do Defender'    = $DefenderStatus.AMServiceVersion
-    'Estado Defender'                  = if ($DefenderStatus.AMServiceEnabledf -eq $true) { 'Habilitado' }else { 'Desabilitado' }
+    'Estado Defender'                  = if ($DefenderStatus.AMServiceEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
+
     # Antivírus - Defender 
     'Estado Antivirus'                 = if ($DefenderStatus.AntivirusEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
     'Ultima atualizacao (Antivirus)'   = $DefenderStatus.AntivirusSignatureLastUpdated
     'Analise de Comportamento'         = if ($DefenderStatus.BehaviorMonitorEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
+
     # Antispyware - Defender
     'Estado Spyware'                   = if ($DefenderStatus.AntispywareEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
     'Ultima atualizacao (Spyware)'     = $DefenderStatus.AntivirusSignatureLastUpdated
+
     # NIS (Network Inspection) - Defender
     'Estado NIS'                       = if ($DefenderStatus.AntivirusEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
     'Ultima atualizacao (NIS)'         = $DefenderStatus.AntivirusSignatureLastUpdated
+
     # Real Time - Defender
     'Estado Protecao em Tempo Real'    = if ($DefenderStatus.RealTimeProtectionEnabled -eq $true) { 'Habilitado' }else { 'Desabilitado' }
+
     # Atualizações do Windwos
     'Ultima atualizacao no OS'         = $LastUpdate
     'Ultima atualizacao de seguranca'  = $LastSecurityUpdate 
     'Lista de HotFix'                  = $HotFixList
 }
 
-$PastaCompartilhada = Get-Location
-Write-Output $Ouptut
-$Ouptut | Export-Csv "$PastaCompartilhada\defender.csv" -Append -NoTypeInformation -UseQuotes AsNeeded
+# Escreve a saída no terminal
+# Write-Output $Ouptut
+
+# Escreve a saída no arquivo 'defender.csv' no caminho de saída
+Export-Csv -InputObject $Ouptut -LiteralPath "$PastaCompartilhada\defender.csv" -Append -NoTypeInformation 
